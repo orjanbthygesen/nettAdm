0c9d2d08b07008e9703fc764b66c625ebba261f2
orjanbthygesen <orjanbthygesen@gmail.com>
2014-03-27 12:32:01 +0100

    sdgfsdfi

---
+++ src/task21/OnlinePeak.java
@@ -0,0 +1,30 @@
+package task21;
+
+public class OnlinePeak {
+	
+	// STATICS 
+	
+	private final static String ipAdressManager = "129.241.209.18";
+	private final static String ipAdressAgent1 = "129.241.209.18";
+	private final static String ipAdressAgent2 = "129.241.209.18";
+	private final static String ipAdressAgent3 = "129.241.209.18";
+	
+	
+	
+	// METHODS
+	
+	private int getNumberOfReceivedDatagrams(String adress){
+		
+		
+		
+		
+		return numberOfReceivedDatagrams;
+		
+	}
+
+	
+	
+	
+	
+
+}
